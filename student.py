64ed94addd57fa72694b6c9c7073a465d4f1282f
Angara-Marie <angaramarie@gmail.com>
2022-05-27 19:59:11 +0300

    added year_of_birth

--- student.py
+++ student.py
@@ -1,14 +1,15 @@
 class Student:
     school = "AkiraChix"
-    def __init__(self, name, year_of_birth):
+    def __init__(self, name, age):
         self.name = name
-        self.year_of_birth = year_of_birth
+        self.age = age
     
     def full_name(self):
         return f"Hello {self.name}."
 
-    def greeting(self):
-        return f"Hello {self.name} you were born in {self.year_of_birth}."
+    def year_of_birth(self):
+        year = 2022 - self.age
+        return f"Hello {self.name} you were born in {year}."
 
     def get_initials(self):
         initials = ""
